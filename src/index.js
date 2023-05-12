bf4547b228285b99d3690d565075b3edaca48c22
Leerena <leerena7@gmail.com>
2023-05-12 19:16:35 +0300

    add hover efects

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './index.html';
+import './page.html';
 
 import 'swiper/scss';
 import 'swiper/scss/pagination';
@@ -6,9 +7,11 @@ import './index.scss';
 import { slidersInit } from './modules/sliders';
 import { videoBgInit } from './modules/videoBg';
 import { menuControl } from './modules/menuControl';
+import { locationHover } from './modules/locationHover';
 
 videoBgInit();
 menuControl();
+locationHover();
 
 slidersInit('.about__slider', {
     pagination: {
@@ -16,12 +19,18 @@ slidersInit('.about__slider', {
     }
 });
 
+const careerImageItems = document.querySelectorAll('.career__image-item');
+
+careerImageItems.forEach((item, i) => {
+  item.classList.add(`career__image-item_${i % 2 ? 'even' : 'odd'}`);
+})
+
 slidersInit('.career__slider', {
     pagination: {
         el: '.career__slider-pagination',
     },
     breakpoints: {
-        768: {
+        576: {
           slidesPerView: 'auto',
           spaceBetween: 20,
         },
